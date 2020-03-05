43b359d7b22c7103922c61eb44f140dd0709783b
Broday Walker <brodaywalker@gmail.com>
2020-03-05 10:12:04 -0600

    insert from input file

--- hashing/main.cpp
+++ hashing/main.cpp
@@ -1,32 +1,31 @@
 #include "hash.h"
 #include <iostream>
+#include <fstream>
 #include <time.h>
 
 using namespace std;
 
 int main()
 {
+    // Declarations
+    int temp;
+
+    // Open input file that holds data for Tuesday's example
+    ifstream infile;
+    infile.open("infile.txt");
+
     // Seed the random number generator
     srand(time(NULL));
 
     // Create a hash table of size 31 for initial testing
-    Hash test(31);
+    Hash table(31);
 
-    test.insert(3);
-    test.insert(9);
-    test.insert(44);
-    test.insert(123);
-    test.insert(99);
-    
-    // Testing wrap
-    test.insert(154);
-    test.insert(154);
-    test.insert(154);
-    test.insert(154);
+    while(infile >> temp)
+        table.insert(temp);
 
 
-    test.Print_Table();
+    table.Print_Table();
 
-
+    infile.close();
     return 0;
 }
