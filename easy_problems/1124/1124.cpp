893614362479ce3efdea25a92fe87b1023bf635c
Broday Walker <brodaywalker@gmail.com>
2020-03-25 18:48:55 -0500

    UVA 1124

---
+++ easy_problems/1124/1124.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string line;
+
+    while(getline(cin, line))
+        cout << line << '\n';
+
+
+
+    return 0;
+}
